aac2b05724c96bdc6df109d9d6eec30e6b6f9ece
Ray <rayrunzewang@gmail.com>
2023-10-02 22:01:41 +1100

    fixed refresh issue

--- server.js
+++ server.js
@@ -144,6 +144,10 @@ app.use('/send-email', emailRoutes);
 //   doc.end();
 // });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Route not found' });
 });
