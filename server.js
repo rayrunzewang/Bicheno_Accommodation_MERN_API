448d125afc7ea4d6abdebe5640bf166757d5ff7d
Ray <rayrunzewang@gmail.com>
2023-10-03 07:26:15 +1100

    minor fix for frontend route

--- server.js
+++ server.js
@@ -143,10 +143,10 @@ app.use('/send-email', emailRoutes);
 //   doc.end();
 // });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public_html')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public_html', 'index.html'));
 });
 
 app.use((req, res, next) => {
